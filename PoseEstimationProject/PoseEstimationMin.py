e2c30eb331deb533dc811f9c97f2ed00b6843586
sebastienschnd <seb.schneider@gmail.com>
2021-07-29 17:46:44 +0200

    PoseEstimation first infos extraction

--- PoseEstimationProject/PoseEstimationMin.py
+++ PoseEstimationProject/PoseEstimationMin.py
@@ -12,9 +12,16 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
-    print(results.pose_landmarks)
+    #print(results.pose_landmarks)
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+            cx, cy = int(lm.x*w), int(lm.y*h)
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+
 
     cTime = time.time()
     fps = 1/(cTime - pTime)
