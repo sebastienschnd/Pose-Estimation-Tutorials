5cf54b42ad916c2607baea4e21677f7d8efdfbe6
sebastienschnd <seb.schneider@gmail.com>
2021-07-30 13:08:39 +0200

    Bounding box styling on face detection

--- FaceDetectionProject/FaceDetectionMin.py
+++ FaceDetectionProject/FaceDetectionMin.py
@@ -17,16 +17,25 @@ while True:
 
     if results.detections:
         for id, detection in enumerate(results.detections):
-            mpDraw.draw_detection(img, detection)
-            print(id, detection)
-            print(detection.score)
-            print(detection.location_data.relative_bounding_box)
+            #mpDraw.draw_detection(img, detection)
+            #print(id, detection)
+            #print(detection.score)
+            #print(detection.location_data.relative_bounding_box)
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                   int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(img, bbox, (255,0,255), 2)
+            cv2.putText(img, f'{int(detection.score[0]*100)}%',
+                        (bbox[0], bbox[1]-20), 
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        2, (0, 255, 0), 2)
    
     cTime = time.time()
     fps = 1/(cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX,
-                1, (0, 255, 0), 2)
+    cv2.putText(img, f'FPS; {int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 255, 0), 2)
     
     cv2.imshow("Image", img)
 
