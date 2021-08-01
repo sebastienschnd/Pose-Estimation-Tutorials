3a3c2b9463c5820041b494eae49b20dfd5783f80
sebastienschnd <seb.schneider@gmail.com>
2021-08-01 16:20:58 +0200

    Creating VolumeHandControl.py with circling thumb and index

---
+++ HandTrackingProject/VolumeHandControl.py
@@ -0,0 +1,38 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+
+########################
+wCam, hCam = 640, 480
+########################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.7)
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[4], lmList[8])
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+
+        cv2.circle(img, (x1, y1), 15, (255,0,255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 15, (255,0,255), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f'FPS: {str(int(fps))}', (40, 50), cv2.FONT_HERSHEY_PLAIN, 
+                1, (255, 0, ), 3)
+
+    cv2.imshow("img", img)
+    cv2.waitKey(1)
