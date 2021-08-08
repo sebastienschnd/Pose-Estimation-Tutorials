2d358c4e0eb528af9683d4e9115c00502b039d75
sebastienschnd <seb.schneider@gmail.com>
2021-08-08 14:12:04 +0200

    AiTrainerProject continuation with not relevant videos

--- PoseEstimationProject/AiTrainerProject.py
+++ PoseEstimationProject/AiTrainerProject.py
@@ -6,16 +6,37 @@ import PoseEstimationModule as pm
 cap = cv2.VideoCapture("videos/a.mp4")
 
 detector = pm.PoseDetector()
+count = 0
+dir = 0
 
 while True:
-    #success, img = cap.read()
-    #img = cv2.resize(img, (1280,720))
-    img = cv2.imread("images/image1.jpg")
-    img = detector.findPose(img)
+    success, img = cap.read()
+    img = cv2.resize(img, (1280,720))
+    #img = cv2.imread("images/image1.jpg")
+    img = detector.findPose(img, False)
     lmList = detector.getPosition(img, False)
     #print(lmList)
     if len(lmList) != 0:
+        # Right Arm
         detector.findAngle(img, 12, 14, 16)
+        # Left Arm
+        angle = detector.findAngle(img, 11, 13, 15)
+        per = np.interp(angle,(210,310),(0,100))
+        #print(angle,per)
+
+        # check the dumbbell curls
+        if per == 100:
+            if dir == 0:
+                count += 0.5
+                dir = 1
+        if per == 0:
+            if dir == 1:
+                count += 0.5
+                dir = 0
+        print(count)
+        cv2.putText(img, f'{count}',(50, 100),
+                    cv2.FONT_HERSHEY_PLAIN,5,(255,0,0),5)
+
 
     cv2.imshow("img", img)
     cv2.waitKey(1)
--- PoseEstimationProject/PoseEstimationModule.py
+++ PoseEstimationProject/PoseEstimationModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp 
 import time
+import math
 
 class PoseDetector:
 
@@ -39,9 +40,18 @@ class PoseDetector:
         return self.lmList
     
     def findAngle(self, img, p1, p2, p3, draw=True):
+        # Get the landmarks
         x1, y1 = self.lmList[p1][1:]
         x2, y2 = self.lmList[p2][1:]
         x3, y3 = self.lmList[p3][1:]
+        #calculate the angle
+        angle = math.degrees(math.atan2(y3-y2,x3-x2)-
+                math.atan2(y1-y2,x1-x2))
+        if angle < 0:
+            angle += 360
+        #print(angle)
+
+        # Draw
         if draw:
             cv2.line(img,(x1,y1),(x2,y2),(255,255,255),3)
             cv2.line(img,(x2,y2),(x3,y3),(255,255,255),3)
@@ -51,6 +61,9 @@ class PoseDetector:
             cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
             cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
             cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)),(x2-20, y2+50),
+                        cv2.FONT_HERSHEY_PLAIN,2,(255,0,255),2)
+        return angle
 
 def main():
     cap = cv2.VideoCapture('videos/a.mp4') #make VideoCapture(0) for webcam
