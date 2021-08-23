6e54401eb9fe19acc7fc16076a776fbe33303d2a
sebastienschnd <seb.schneider@gmail.com>
2021-08-23 17:01:12 +0200

    Drawing lines implementation

--- HandTrackingProject/VirtualPaint.py
+++ HandTrackingProject/VirtualPaint.py
@@ -4,6 +4,11 @@ import time
 import os
 import HandTrackingModule as htm 
 
+#####################################
+brushThickness = 15
+
+#####################################
+
 folderPath = "Brushes"
 myList = os.listdir(folderPath)
 print(myList)
@@ -13,12 +18,15 @@ for imPath in myList:
     overlayList.append(image)
 print(len(overlayList))   
 header = overlayList[0]
+drawColor = (255,0,255)
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 detector = htm.handDetector(detectionCon=0.85)
+xp, yp = 0, 0
+imgCanvas = np.zeros((480,640,3),np.uint8)
 
 while True:
 
@@ -43,25 +51,38 @@ while True:
 
         # 4. If selection mode - Two finger are up
         if fingers[1] and fingers[2]:
-            cv2.rectangle(img, (x1,y1-25), (x2,y2+25),(255,0,255),cv2.FILLED)
+            
             print("selection Mode")
             # Checking for the click
             if y1<125:
                 if 250<x1<50:
                     header = overlayList[0]
+                    drawColor = (255, 0, 255)
                 elif 550<x1<750:
                     header = overlayList[1]
+                    drawColor = (255, 0, 0)
                 elif 800<x1<950:
                     header = overlayList[2]
+                    drawColor = (0, 255, 0)
                 elif 1050<x1<1200:
                     header = overlayList[3]
+                    drawColor = (0, 0, 0)
+            cv2.rectangle(img, (x1,y1-25), (x2,y2+25),drawColor, cv2.FILLED)
 
         # 5. If drawing Mode - Index finger is up
         if fingers[1] and fingers[2]==False:
-            cv2.circle(img,(x1,y1),15,(255,0,255), cv2.FILLED)
+            cv2.circle(img,(x1,y1),15, drawColor, cv2.FILLED)
             print("Drawing Mode")
+            if xp==0 and yp==0:
+                xp, yp = x1, y1
+
+            cv2.line(img, (xp,yp), (x1,y1),drawColor,brushThickness)
+            cv2.line(imgCanvas, (xp,yp), (x1,y1),drawColor,brushThickness)
+
+            xp, yp = x1, y1
 
     # Setting the header image
     img[0:125, 0:640] = header[0:125, 0:640]
-    cv2.imshow("image", img)
+    cv2.imshow("Image", img)
+    cv2.imshow("Canvas", imgCanvas)
     cv2.waitKey(1)
