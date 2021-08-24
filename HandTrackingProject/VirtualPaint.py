dec77f4a6134a1d8eb15c383b7e5ddd2b5551f1e
sebastienschnd <seb.schneider@gmail.com>
2021-08-24 15:06:00 +0200

    Ending virtualPaint project

--- HandTrackingProject/VirtualPaint.py
+++ HandTrackingProject/VirtualPaint.py
@@ -6,7 +6,7 @@ import HandTrackingModule as htm
 
 #####################################
 brushThickness = 15
-
+eraserThickness = 50
 #####################################
 
 folderPath = "Brushes"
@@ -39,6 +39,7 @@ while True:
     lmList = detector.findPosition(img, draw=False)
 
     if len(lmList) != 0:
+
         #print(lmList)
 
         # tip of index and middle fingers
@@ -51,20 +52,20 @@ while True:
 
         # 4. If selection mode - Two finger are up
         if fingers[1] and fingers[2]:
-            
+            xp, yp = 0, 0
             print("selection Mode")
             # Checking for the click
             if y1<125:
-                if 250<x1<50:
+                if 160>x1>0:
                     header = overlayList[0]
                     drawColor = (255, 0, 255)
-                elif 550<x1<750:
+                elif 320>x1>161:
                     header = overlayList[1]
                     drawColor = (255, 0, 0)
-                elif 800<x1<950:
+                elif 480>x1>321:
                     header = overlayList[2]
                     drawColor = (0, 255, 0)
-                elif 1050<x1<1200:
+                elif 640>x1>481:
                     header = overlayList[3]
                     drawColor = (0, 0, 0)
             cv2.rectangle(img, (x1,y1-25), (x2,y2+25),drawColor, cv2.FILLED)
@@ -76,13 +77,24 @@ while True:
             if xp==0 and yp==0:
                 xp, yp = x1, y1
 
-            cv2.line(img, (xp,yp), (x1,y1),drawColor,brushThickness)
-            cv2.line(imgCanvas, (xp,yp), (x1,y1),drawColor,brushThickness)
+            if drawColor == (0,0,0):
+                cv2.line(img, (xp,yp), (x1,y1),drawColor,eraserThickness)
+                cv2.line(imgCanvas, (xp,yp), (x1,y1),drawColor,eraserThickness)
+            else:
+                cv2.line(img, (xp,yp), (x1,y1),drawColor,brushThickness)
+                cv2.line(imgCanvas, (xp,yp), (x1,y1),drawColor,brushThickness)
 
             xp, yp = x1, y1
 
+    imgGray = cv2.cvtColor(imgCanvas,cv2.COLOR_BGR2GRAY) 
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)     
+    imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img,imgInv)
+    img = cv2.bitwise_or(img,imgCanvas)
+
     # Setting the header image
     img[0:125, 0:640] = header[0:125, 0:640]
+    #img = cv2.addWeighted(img,0.5,imgCanvas,0.5,0)
     cv2.imshow("Image", img)
     cv2.imshow("Canvas", imgCanvas)
     cv2.waitKey(1)
